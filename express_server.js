3f882f6cbd4ab9f8908379e6c9555cb5f50f3bc1
granttaylor448 <granttaylor448@gmail.com>
2019-12-05 21:15:42 +0000

    tweaked express server

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.port || 8080; // default port 8080
+const PORT = process.env.PORT || 8080; // default port 8080
 const ENV = process.env.ENV || "development";
 
 app.set("view engine", "ejs");
